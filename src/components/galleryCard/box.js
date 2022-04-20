55aad20daf8c76daa5789ee0ec08ebd1c657d95b
RahmaRamadan <rahmaramadan23@gmailcom>
2022-04-20 03:50:47 +0200

    merge modal and carousel on box component

--- src/components/galleryCard/box.js
+++ src/components/galleryCard/box.js
@@ -1,36 +1,63 @@
-import React from "react";
+import React,{useState} from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import icon from "../../static/zoom.png";
 import "./box.css";
-
+import Modal from "./modal/modal";
+import './popup/popup.css'
 export default function Box(props) {
-  console.log("iconn: ", icon);
-  return (
-    <div className="container">
-      <div className="row">
-        {props.photos.map((photo, index) => {
-          return (
-            <div
-              key={photo.uuid}
-              className="container mt-4 col-lg-3 col-xs-4 col-sm-6"
-            >
-              <div className="row container-one">
-                <img
-                  src={photo.url}
-                  alt={photo.name}
-                  height={250}
-                  width={250}
-                />
+  
+  let [photoClicked, setPhoto] = useState();
+  let [counterClicked, setCounter] = useState();
 
-                <a href="#" className="hidden_img">
-                  <img src={icon} alt="zoom icon" height={50} width={50} />
-                </a>
+  const resetCounter = (photoState, index) => {
+    setPhoto(photoState);
+    setCounter(index);
+    console.log("hello from clickedd")
+  }
+  return (
+    <>
+
+      <div className="container">
+        <div className="row">
+
+          {props.photos.map((photo, index) => {
+            return (
+              <div
+                key={photo.uuid}
+                className="container mt-4 col-lg-4 col-xs-4 col-sm-6"
+              >
+                <div className="row container-one">
+                  <img
+                    src={photo.url}
+                    alt={photo.name}
+                    height={250}
+                    width={250}
+                  />
+
+                  <a href="#popup1" className="hidden_img" onClick={() => resetCounter(photo, index)}>
+                    <img
+                      src={icon}
+                      alt="zoom icon"
+                      data-modal="true"
+                      height={50}
+                      width={50}
+                    />
+                  </a>
+                </div>
+               
               </div>
-              {/* <div className="card-body">{photo.name}</div> */}
-            </div>
-          );
-        })}
+            );
+          })}
+           <div id="popup1" className="overlay">
+                  <div className="popup">
+                    <a className="close" href="/">&times;</a>
+                    <div className="container">
+                      <Modal photos={props.photos} init_counter={counterClicked} init_photo={photoClicked} />
+                    </div>
+                  </div>
+                </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
