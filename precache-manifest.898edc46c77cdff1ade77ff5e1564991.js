3d1672e43e18609f2d904836737031c770894047
RahmaRamadan <rahmaramadan23@gmailcom>
2022-04-20 04:46:53 +0200

    Updates

---
+++ precache-manifest.898edc46c77cdff1ade77ff5e1564991.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a3e4827aa20240a93c7",
+    "url": "/GalleryApp/static/css/main.10846593.chunk.css"
+  },
+  {
+    "revision": "3a3e4827aa20240a93c7",
+    "url": "/GalleryApp/static/js/main.3a3e4827.chunk.js"
+  },
+  {
+    "revision": "f0c94a440c9aa679f40d",
+    "url": "/GalleryApp/static/css/1.2069b27e.chunk.css"
+  },
+  {
+    "revision": "f0c94a440c9aa679f40d",
+    "url": "/GalleryApp/static/js/1.f0c94a44.chunk.js"
+  },
+  {
+    "revision": "042ccd9a60ddda5377ff",
+    "url": "/GalleryApp/static/js/runtime~main.042ccd9a.js"
+  },
+  {
+    "revision": "96d166e0365e2187633797fec5bd3345",
+    "url": "/GalleryApp/static/media/zoom.96d166e0.png"
+  },
+  {
+    "revision": "2557f1c47824a645d40ad6122d8fd370",
+    "url": "/GalleryApp/index.html"
+  }
+];
