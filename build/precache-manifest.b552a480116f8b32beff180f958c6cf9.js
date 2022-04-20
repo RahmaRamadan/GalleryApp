f4289f119464857fe1f98c3397e25ed09cbf2aae
RahmaRamadan <rahmaramadan23@gmailcom>
2022-04-20 22:19:47 +0200

    merge with main branch

---
+++ build/precache-manifest.b552a480116f8b32beff180f958c6cf9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "53e300c58bc0b3de079a",
+    "url": "/GalleryApp/static/css/main.7fa8bb00.chunk.css"
+  },
+  {
+    "revision": "53e300c58bc0b3de079a",
+    "url": "/GalleryApp/static/js/main.53e300c5.chunk.js"
+  },
+  {
+    "revision": "f0c94a440c9aa679f40d",
+    "url": "/GalleryApp/static/css/1.2069b27e.chunk.css"
+  },
+  {
+    "revision": "f0c94a440c9aa679f40d",
+    "url": "/GalleryApp/static/js/1.f0c94a44.chunk.js"
+  },
+  {
+    "revision": "042ccd9a60ddda5377ff",
+    "url": "/GalleryApp/static/js/runtime~main.042ccd9a.js"
+  },
+  {
+    "revision": "96d166e0365e2187633797fec5bd3345",
+    "url": "/GalleryApp/static/media/zoom.96d166e0.png"
+  },
+  {
+    "revision": "3d1d61fbc686fa21ba8ed312298290df",
+    "url": "/GalleryApp/index.html"
+  }
+];
